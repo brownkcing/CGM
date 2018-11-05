075c2e561559f7236ef49fca3a1676af74997657
brownkcing <brownkcing@gmail.com>
2018-11-05 15:10:09 +0400

    revamped portfolio, using template

---
+++ script.js
@@ -0,0 +1,9 @@
+//sidebars
+function openNav() {
+    document.getElementById("mySidebar").style.width = "60%";
+    document.getElementById("mySidebar").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.display = "none";
+}
